7000850a2ef50ba3d6febd9dc33415db85524ac6
Grace Njenga <76649048+Grace-Njenga@users.noreply.github.com>
2024-02-22 12:22:29 +0300

    swiper

--- dist/script.js
+++ dist/script.js
@@ -13,21 +13,38 @@ navToogle.addEventListener("click", () => {
    }
 });
 
-// initialize aos
-AOS.init();
-
 // swiper js
 const swiper = new Swiper(".swiper", {
    // Optional parameters
-   direction: isLargeScreen ? "vertical" : "horizontal",
+   direction: "vertical",
    slidesPerView: 1,
    spaceBetween: 30,
    effect: "slide",
    mousewheel: isLargeScreen,
    loop: false,
+
    // If we need pagination
    pagination: {
       el: ".swiper-pagination",
       clickable: true,
    },
 });
+// Check if the screen width is greater than a certain value
+// const isLargeScreen = window.innerWidth > 768;
+// const swiper = new Swiper(".swiper", {
+//    // Optional parameters
+//    direction: isLargeScreen ? "vertical" : "horizontal",
+//    slidesPerView: 1,
+//    spaceBetween: 30,
+//    effect: "slide",
+//    mousewheel: isLargeScreen,
+//    loop: false,
+//    // If we need pagination
+//    pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//    },
+// });
+
+// initialize aos
+AOS.init();
