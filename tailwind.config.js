a7466a2ae1acfe01c35275df0ce8a996278d1399
Grace Njenga <76649048+Grace-Njenga@users.noreply.github.com>
2024-06-10 18:29:18 +0300

    small changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,20 +10,36 @@ module.exports = {
     },
 
     colors: {
-      primary: "#287a28",
-      secondary: "#d6f4ff",
-      accent: "#007348",
-      // green 
-      main: "#003500",
-      hover: "#ffd885",
-      white: "#f8f2f2"
-   },
+      // DARK-GREEN ---- Primary color
+      // Can be used as a primary color, background color for sections, or in elements like buttons, headers, and footers. 
+      dark_green__primary: "#638A55",
+
+      // light-green ---- Accent color
+      // Can be used as an accent color, secondary background color, or for highlights and call-to-action buttons.
+      light_green__secondary: "light-green",
+
+      // Soft Yellow-Green---- highlighting CTAs
+      //  Can be used for highlighting important information, backgrounds, or in graphical elements and icons.
+      light_yellow__highlight: "#CDDB86",
+
+      // Pale Yellow --- Background color
+      // Can be used as a background color for content sections, or to add a soft, light feel to certain elements.
+      pale_yellow__background: "#FCE7B2",
+
+      // light-brown ---- Complementary color
+      // Can be used for backgrounds, borders, or as a complementary color to create a natural, earthy feel.
+      light_brown__background: "#E1B083",
+
+      //  Medium-brown ---- Secondary color
+      // Can be used for text, headings, or as a secondary color for creating contrast with lighter background colors.
+      medium_brown__complementary: "#C48D60",
+    },
     extend: {
       height: {
-        '500': 'min',
-      }
+        500: "min",
+      },
     },
   },
   plugins: [],
-}
+};
 
