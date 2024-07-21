8896c0a3e17712de652ce9e0571d7c862e5045d0
Grace Njenga <76649048+Grace-Njenga@users.noreply.github.com>
2024-07-21 15:50:53 +0300

    our products

--- dist/script.js
+++ dist/script.js
@@ -2,17 +2,55 @@
 const primaryNav = document.querySelector(".nav");
 const navToogle = document.querySelector(".menu");
 navToogle.addEventListener("click", () => {
-   const visibility = primaryNav.getAttribute("data-visible");
+  const visibility = primaryNav.getAttribute("data-visible");
 
-   if (visibility === "false") {
-      primaryNav.setAttribute("data-visible", true);
-      navToogle.setAttribute("aria-expanded", true);
-   } else if (visibility === "true") {
-      primaryNav.setAttribute("data-visible", false);
-      navToogle.setAttribute("aria-expanded", false);
-   }
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToogle.setAttribute("aria-expanded", true);
+  } else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToogle.setAttribute("aria-expanded", false);
+  }
 });
 
+// Initialize swipper
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  centeredSlides: true,
+  // Optional parameters
+  direction: "horizontal",
+  //   responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+
+  //  CoverOverflow effect
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: 30,
+    slideShadows: false,
+  },
+});
 
 // initialize aos
 AOS.init();
